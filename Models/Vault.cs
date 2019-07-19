1aa734c94a09fc71aa35da057cb4281c5f948169
Connor Kennedy <kennedyconnor02@gmail.com>
2019-07-19 16:22:47 -0600

    models

---
+++ Models/Vault.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace keepr.Models
+{
+  public class Vault
+  {
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Description { get; set; }
+    public string userId { get; set; }
+
+  }
+}
